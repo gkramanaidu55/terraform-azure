39c3011c5041503b6f8ea9f07a5e514dff87a81f
gkramanaidu55 <gkramanaidu55@gmail.com>
2026-03-28 13:48:43 -0400

    Delete Modules directory

--- Modules/compute/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "location" {}
-variable "rg_name" {}
-variable "subnet_id" {}
-variable "vm_size" {}
-variable "admin_username" {}
-variable "admin_password" {}
--- Modules/network/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "location" {}
-variable "rg_name" {}
